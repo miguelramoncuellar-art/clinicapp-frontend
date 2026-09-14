dd6294ea3c4672d39c9b917bec68a7ab3aebe719
Miguel Ramon Cuellar <miguel.ramoncuellar@gmail.com>
2026-09-14 18:40:17 -0500

    style: vistas responsive de dashboard, productos y formulario

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://192.168.0.9:3000/api',
+  apiUrl: 'http://192.168.0.13:3000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // IP local del PC en la red Wi-Fi. Si el router reasigna otra IP, actualizar aqui.
   // TODO: reemplazar por la URL de produccion cuando exista
-  apiUrl: 'http://192.168.0.9:3000/api',
+  apiUrl: 'http://192.168.0.13:3000/api',
 };
